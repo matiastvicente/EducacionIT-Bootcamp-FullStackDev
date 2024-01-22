f4c95af0ba9108b0a571d9b356b72938571510de
matiastvicente <matiastvicente@gmail.com>
2024-01-22 17:16:22 -0300

    22 de enero

---
+++ Clases/Clase 15/Javascript0/acceso.js
@@ -0,0 +1,6 @@
+console.log("Soy el script externo de acceso");
+var refparrafo = document.querySelector("p");
+console.log(refparrafo);
+
+refparrafo.style.backgroundColor = "cyan";
+refparrafo.style.padding = "10px";
