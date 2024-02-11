83402c901c819712d0be5a87cd1981598d1f9b33
matiastvicente <matiastvicente@gmail.com>
2024-02-11 17:13:13 -0300

    Ejercicio 7 terminado

---
+++ Ejercicios/Ejercicio 7/js/main.js
@@ -0,0 +1,65 @@
+/* Variables */
+
+var p = document.querySelector("p");
+var btn = document.querySelectorAll("button");
+var ArrayEmojis = ["😺", "😸", "😹", "⬛"];
+var ArrayGatos = [];
+var control0 = 0;
+var control1 = 0;
+var control2 = 0;
+var numCajas = 0;
+
+/* Funciones */
+
+function start() {
+  console.log("Ejecutando: " + document.querySelector("title").innerText);
+  btn[0].onclick = gato0;
+  btn[1].onclick = gato1;
+  btn[2].onclick = gato2;
+}
+
+function gato0() {
+  control0++;
+  control1 = control2 = 0;
+  ArrayGatos.push(ArrayEmojis[0]);
+  p.innerText = ArrayGatos.join("");
+  if (control0 == 6) {
+    caja();
+    control0 = 0;
+  }
+}
+
+function gato1() {
+  control1++;
+  control0 = control2 = 0;
+  ArrayGatos.push(ArrayEmojis[1]);
+  p.innerText = ArrayGatos.join("");
+  if (control1 == 6) {
+    caja();
+    control1 = 0;
+  }
+}
+
+function gato2() {
+  control2++;
+  control0 = control1 = 0;
+  ArrayGatos.push(ArrayEmojis[2]);
+  p.innerText = ArrayGatos.join("");
+  if (control2 == 6) {
+    caja();
+    control2 = 0;
+  }
+}
+
+function caja() {
+  while (ArrayGatos.length > numCajas) {
+    ArrayGatos.pop();
+  }
+  ArrayGatos.unshift(ArrayEmojis[3]);
+  p.innerText = ArrayGatos.join("");
+  numCajas++;
+}
+
+/* Programa */
+
+start();
